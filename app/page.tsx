385d328974dddd635baedbac4b6612261ff0779e
Gliga Dumitru <gliga_dumitru@yahoo.com>
2026-01-19 19:00:11 +0200

    chore: update package dependencies, add typography plugin, and enhance global styles with new utility classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-primary-500 text-3xl font-bold">Hello World2</h1>
+      <h2 className="font-space-grotesk font-medium">Hello World</h2>
+      <h3 className="h3-bold">Hello World</h3>
+      <h4 className="h4-bold">Hello World</h4>
+      <h5 className="h5-bold">Hello World</h5>
+      <h6 className="h6-bold">Hello World</h6>
+      <p className="paragraph-regular">Hello World</p>
+      <p className="paragraph-medium">Hello World</p>
+      <p className="paragraph-semibold">Hello World</p>
     </div>
   );
 };
