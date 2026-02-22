67b208bc5e92dc942ed7c75f94f335b35d320580
Gliga Dumitru <gliga_dumitru@yahoo.com>
2026-02-22 10:46:01 +0200

    feat: add Interaction model for MongoDB with Mongoose

---
+++ database/interaction.model.ts
@@ -0,0 +1,28 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export type TargetType = "question" | "answer";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionType: TargetType;
+  actionId: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: {
+      type: String,
+      required: true,
+    },
+    actionType: { type: String, required: true, enum: ["question", "answer"] },
+    actionId: { type: Schema.Types.ObjectId, required: true },
+  },
+  { timestamps: true }
+);
+
+const Interaction = models?.interaction || model<IInteraction>("Interaction", InteractionSchema);
+export default Interaction;
