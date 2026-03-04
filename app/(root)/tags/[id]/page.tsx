837ab73f55959a071f7a165202d8ef1324c41dfe
Gliga Dumitru <gliga_dumitru@yahoo.com>
2026-03-04 19:11:48 +0200

    feat: enhance question detail view with new components and utilities
    - Introduced a new Preview component for rendering markdown content with syntax highlighting.
    - Added Metric component to display question statistics such as views, answers, and timestamps.
    - Implemented utility functions for formatting numbers and timestamps to improve data presentation.
    - Updated the QuestionDetails component to utilize new components for a more structured and visually appealing layout.
    - Enhanced the TagByIdPage to include dynamic search functionality and improved question rendering based on tags.

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,9 +1,51 @@
-const TagByIdPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+import QuestionCard from "@/components/cards/QuestionCard";
+import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getTagQuestions } from "@/lib/actions/tag.action";
+
+const TagByIdPage = async ({ params, searchParams }: RouteParams) => {
   const { id } = await params;
+  const { page, pageSize, query } = await searchParams;
+
+  const { success, data, error } = await getTagQuestions({
+    tagId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+  });
+
+  const { tag, questions = [] } = data || {};
 
   return (
     <>
-      <h1>Tag {id}</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        {tag && (
+          <div className="flex items-center gap-2">
+            <h1 className="h1-bold text-dark100_light900">{tag?.name}</h1>
+            <TagCard key={tag._id} _id={tag._id} name={tag.name} compact />
+          </div>
+        )}
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAG(id)}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <DataRenderer
+        success={success}
+        data={questions ?? []}
+        error={error}
+        empty={EMPTY_QUESTION}
+        render={(questions) =>
+          questions.map((question) => <QuestionCard key={question._id.toString()} question={question} />)
+        }
+      />
     </>
   );
 };
