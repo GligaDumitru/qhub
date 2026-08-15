b642918a02948c780e1a999fd85592282465138d
Gliga Dumitru <gliga_dumitru@yahoo.com>
2026-08-15 09:57:54 +0300

    feat: update navigation components and global styles for improved dark mode support

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -11,7 +11,7 @@ const LeftSidebar = async () => {
   const userId = session?.user?.id;
 
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border shadow-light-300 sticky top-0 left-0 z-30 flex h-screen min-h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-[266px] dark:shadow-none">
+    <section className="custom-scrollbar background-light900_dark-nav-bg light-border shadow-light-300 sticky top-0 left-0 z-30 flex h-screen min-h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-[266px] dark:shadow-none">
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks userId={userId} />
       </div>
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <nav className="flex-between background-light900_dark200 fixed top-0 z-50 w-full p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark-nav-bg shadow-light200_darknone border-b-light300_darknone fixed top-0 z-50 w-full border p-6 sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image src="/images/site-logo.svg" alt="QHub Logo" width={23} height={23} />
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
