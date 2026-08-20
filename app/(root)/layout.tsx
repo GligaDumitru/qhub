7e07b61f4e7a55fbb517a5947f1ba9e0b89343be
Gliga Dumitru <gliga_dumitru@yahoo.com>
2026-08-20 19:30:11 +0300

    feat: enhance layout and sidebar components for improved responsiveness and styling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,11 +5,11 @@ import "@mdxeditor/editor/style.css";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="background_light850_dark100 relative">
+    <main className="background_light850_dark100 relative flex h-screen flex-col overflow-hidden">
       <Navbar />
-      <div className="flex">
+      <div className="flex flex-1 overflow-hidden">
         <LeftSidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-full flex-1 flex-col overflow-scroll p-6 max-md:pb-14 sm:px-14">
           <div className="m-w-5xl mx-auto w-full">{children}</div>
         </section>
 
